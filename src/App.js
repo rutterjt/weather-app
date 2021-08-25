2630c380c53616eab01adcad8dc6e5a06c3dc2d6
Jon Rutter <contact@jonrutter.com>
2021-08-25 17:50:19 -0400

    Add loading functionality to App.js

--- src/App.js
+++ src/App.js
@@ -13,26 +13,33 @@ import data from 'data.json';
 
 const App = () => {
   const [current, setCurrent] = useState({});
-  const [hourlyForecast, setHourlyForecast] = useState([]);
-  const [dailyForecast, setDailyForecast] = useState([]);
+  const [hourlyForecast, setHourlyForecast] = useState({});
+  const [dailyForecast, setDailyForecast] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     setCurrent(data.current);
     setHourlyForecast(data.hourly);
     setDailyForecast(data.daily);
+    setLoading(false);
   }, []);
 
-  return (
-    <Wrapper>
-      <Header />
-      <main>
-        <Overview forecast={current} />
-        <Hourly forecast={hourlyForecast} />
-        <Daily forecast={dailyForecast} />
-        <Today forecast={current} />
-      </main>
-    </Wrapper>
-  );
+  if (loading) {
+    return <h1>Loading</h1>;
+  } else {
+    return (
+      <Wrapper>
+        <Header />
+        <main>
+          <Overview forecast={current} />
+          <Hourly forecast={hourlyForecast} />
+          <Daily forecast={dailyForecast} />
+          <Today forecast={current} />
+        </main>
+      </Wrapper>
+    );
+  }
 };
 
 export default App;
