e9d7f2ec472793dc49d8310190633876513f0358
Jon Rutter <contact@jonrutter.com>
2021-08-25 17:50:00 -0400

    Overview component: display image based on current weather data

--- src/layout/Overview/index.js
+++ src/layout/Overview/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+// helper functions
+import { getTimeOfDay } from 'helpers/time-of-day';
+
 // styles
 import s from './Overview.module.css';
 
-const Overview = () => {
-  return null;
+import WeatherImage from 'components/WeatherImage';
+
+const Overview = ({ forecast }) => {
+  const code = forecast.weather[0].id;
+  const timeOfDay = getTimeOfDay(forecast.sunrise, forecast.sunset);
+  return (
+    <div>
+      <WeatherImage code={code} timeOfDay={timeOfDay} className={s.image} />
+    </div>
+  );
 };
 
 export default Overview;
