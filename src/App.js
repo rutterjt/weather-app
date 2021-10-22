de341aee6e6c26939993dc9a2dc1ee25a45d3457
jonrutter <contact@jonrutter.com>
2021-10-22 16:00:31 -0400

    Server request throttling

--- src/App.js
+++ src/App.js
@@ -3,61 +3,59 @@ import React, { useEffect, useState } from 'react';
 // components
 import Wrapper from 'layout/Wrapper';
 import Header from 'layout/Header';
-import Overview from 'layout/Overview';
-import Hourly from 'layout/Hourly';
-import Daily from 'layout/Daily';
-import Today from 'layout/Today';
+// import Overview from 'layout/Overview';
+// import Hourly from 'layout/Hourly';
+// import Daily from 'layout/Daily';
+// import Today from 'layout/Today';
 
 // data
-import data from 'data.json';
+// import data from 'data.json';
 
 import { geolocationFetch } from 'api';
 
+const isStale = (time) => {
+  return Date.now() > time + 300000;
+};
+
+const saveWeather = (data) => {
+  localStorage.setItem('weather', JSON.stringify(data));
+  localStorage.setItem('time', JSON.stringify(Date.now()));
+};
+
+const getItem = (item) => localStorage.getItem(item);
+const read = (item) => JSON.parse(getItem(item));
+const readWeather = () => read('weather');
+const readTime = () => read('time');
+
 const App = () => {
-  const [current, setCurrent] = useState({});
-  const [hourlyForecast, setHourlyForecast] = useState({});
-  const [dailyForecast, setDailyForecast] = useState({});
-  const [alert, setAlert] = useState([]);
-
-  const [weather, setWeather] = useState('');
+  const [weather, setWeather] = useState(() => {
+    return localStorage.getItem('weather') ? readWeather() : '';
+  });
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const data = geolocationFetch();
-    if (!data) {
-      setError('Could not get current position');
-      console.log('Could not get current position');
-    } else {
-      setWeather(data);
+    if (!getItem('time') || isStale(readTime())) {
+      setLoading(true);
+      geolocationFetch('weather')
+        .then((data) => {
+          setWeather(data);
+          saveWeather(data);
+          setError('');
+        })
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => setLoading(false));
     }
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    setLoading(true);
-
-    setCurrent(data.current);
-    setHourlyForecast(data.hourly);
-    setDailyForecast(data.daily);
-    if (data.alerts) {
-      setAlert(data.alerts[0]);
-    }
-    setLoading(false);
   }, []);
 
   if (loading) {
-    return <h1>Loading</h1>;
+    return <h1 style={{ color: 'black' }}>Loading</h1>;
   } else {
     return (
       <Wrapper>
         <Header />
-        <main>
-          <Overview forecast={current} alert={alert} />
-          <Hourly forecast={hourlyForecast} />
-          <Daily forecast={dailyForecast} />
-          <Today forecast={current} />
-        </main>
       </Wrapper>
     );
   }
