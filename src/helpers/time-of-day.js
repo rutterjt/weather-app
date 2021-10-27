4d93f8b19309d8908157f911680b82acc02cd3ce
jonrutter <contact@jonrutter.com>
2021-10-26 20:21:41 -0400

    Update getTimeOfDay helper function

--- src/helpers/time-of-day.js
+++ src/helpers/time-of-day.js
@@ -1,7 +1,7 @@
 // takes two arguments: time of sunrise and time of sunset, in seconds
 // returns "day", "night", or "twilight" depending on the time of day
 export const getTimeOfDay = (sunrise, sunset) => {
-  const current = Math.floor(Date.now() / 1000); // the current time, in seconds, Unix time
+  const current = Math.floor(new Date().getTime() / 1000); // the current time, in seconds, Unix time
   // if the current time is earlier than 30 minutes before sunrise, or later than 30 minutes after sunset: it is night
   if (current < sunrise - 1800 || current > sunset + 1800) return 'night';
   // if the current time is both: later than 30 minutes after sunrise, and earler than 30 minutes before sunset: it is day
