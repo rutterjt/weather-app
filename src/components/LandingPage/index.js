74da287397c95f24a569ad3757d7574aae2cd946
jonrutter <contact@jonrutter.com>
2021-10-27 19:26:22 -0400

    Add load overlay, weather fetch button, and error handling to LandingPage

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -6,15 +6,26 @@ import * as s from './LandingPage.module.css';
 import Layout from 'components/Layout';
 import Header from 'components/Header';
 import WeatherImage from 'components/WeatherImage';
+import LocationButton from 'components/LocationButton';
 
-const LandingPage = () => {
+const LandingPage = ({ fetchWeather, error, loading }) => {
   const weatherId = 100;
   const timeOfDay = 'day';
   return (
-    <Layout weatherID={weatherId} timeOfDay={timeOfDay}>
-      <Header title="Weather App" />
-      <div className={s.iconWrap}>
-        <WeatherImage weatherID={weatherId} timeOfDay={timeOfDay} />
+    <Layout
+      weatherID={weatherId}
+      timeOfDay={timeOfDay}
+      loading={loading}
+      header={<Header title="Weather App" />}
+    >
+      <div>
+        <div className={s.iconWrap}>
+          <WeatherImage weatherID={weatherId} timeOfDay={timeOfDay} />
+        </div>
+        <div className={s.btnWrap}>
+          <LocationButton label="Get Weather" callback={fetchWeather} />
+          {error && <p>Oops, there was an error. Please try again.</p>}
+        </div>
       </div>
     </Layout>
   );
