b3204a6365f749ab29fd986881eedae1b65fefb2
jonrutter <contact@jonrutter.com>
2021-10-27 19:28:56 -0400

    Improve error handling, render snow/rain volume

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -6,8 +6,9 @@ import * as s from './Details.module.css';
 // helpers
 import { getDayOfWeek, getHourAndMinute, getMonthName } from 'helpers/date';
 import { getWindDirection } from 'helpers/format';
-import { getInHg } from 'helpers/format';
+import { getInHg, mmToIn } from 'helpers/format';
 
+// helper component: renders styled symbol for degrees fahrenheit,
 const F = () => {
   return (
     <span>
@@ -16,7 +17,7 @@ const F = () => {
   );
 };
 
-const Details = ({ weather, main, sys, wind, clouds }) => {
+const Details = ({ weather, main, sys, wind, clouds, rain, snow }) => {
   const { description } = weather[0];
   const { temp, feels_like: feelsLike, pressure, humidity } = main;
   const { sunrise, sunset } = sys;
@@ -25,78 +26,101 @@ const Details = ({ weather, main, sys, wind, clouds }) => {
   const today = new Date();
   const now = today / 1000;
 
+  const has = (prop) => prop !== undefined;
+
   return (
     <div className={s.wrapper}>
-      <div className={s.content}>
-        <h2 className={s.title}>Today's Details</h2>
-        <ul className={s.list}>
-          <li>
+      <h2 className={s.title}>Today's Details</h2>
+      <ul className={s.list}>
+        <li>
+          <div>
             {getDayOfWeek(now)}, {getMonthName(now)} {today.getDate()},{' '}
-            {getHourAndMinute(now)}
-          </li>
-          <li>
+            {getHourAndMinute(now)}.
+          </div>
+          <div>
             Currently {Math.round(temp)}
             <F />, {description}.
-          </li>
-          <li>Sunrise: {getHourAndMinute(sunrise)}</li>
-          <li>Sunset: {getHourAndMinute(sunset)}</li>
-        </ul>
+          </div>
+        </li>
+        <li className={s.flexLi}>
+          <div className={s.liBox}>
+            <span>Sunrise:</span>
+            <span>{getHourAndMinute(sunrise)}</span>
+          </div>
+          <div className={s.liBox}>
+            <span>Sunset:</span>
+            <span>{getHourAndMinute(sunset)}</span>
+          </div>
+        </li>
+      </ul>
 
-        <h3 className={s.subtitle}>Current Conditions</h3>
-        <ul className={s.detailsList}>
-          {temp && (
-            <li>
-              <span>Temperature: </span>
-              <span>
-                {Math.round(temp)}
-                <F />
-              </span>
-            </li>
-          )}
-          {feelsLike && (
-            <li>
-              <span>Feels Like: </span>
-              <span>
-                {Math.round(feelsLike)}
-                <F />
-              </span>
-            </li>
-          )}
-          {windSpeed && (
-            <li>
-              <span>Wind: </span>
-              <span>
-                {windDirection && getWindDirection(windDirection)}, {windSpeed}{' '}
-                mph
-              </span>
-            </li>
-          )}
-          {gust && (
-            <li>
-              <span>Wind Gusts: </span>
-              <span>{gust} mph</span>
-            </li>
-          )}
-          {humidity && (
-            <li>
-              <span>Humidity: </span>
-              <span>{humidity}%</span>
-            </li>
-          )}
-          {pressure && (
-            <li>
-              <span>Pressure: </span>
-              <span>{Math.round(getInHg(pressure) * 100) / 100} inHg</span>
-            </li>
-          )}
-          {cloudCover && (
-            <li>
-              <span>Cloud cover: </span>
-              <span>{cloudCover}%</span>
-            </li>
-          )}
-        </ul>
-      </div>
+      <h3 className={s.subtitle}>Current Conditions</h3>
+      <ul className={s.detailsList}>
+        {has(temp) && (
+          <li>
+            <span>Temperature: </span>
+            <span>
+              {Math.round(temp)}
+              <F />
+            </span>
+          </li>
+        )}
+        {has(feelsLike) && (
+          <li>
+            <span>Feels Like: </span>
+            <span>
+              {Math.round(feelsLike)}
+              <F />
+            </span>
+          </li>
+        )}
+        {has(humidity) && (
+          <li>
+            <span>Humidity: </span>
+            <span>{humidity}%</span>
+          </li>
+        )}
+        {has(rain) && has(rain['1h']) && (
+          <li>
+            <span>Rainfall (last hour): </span>
+            <span>{mmToIn(rain['1h'])}in</span>
+          </li>
+        )}
+        {has(snow) && has(snow['1h']) && (
+          <li>
+            <span>Snowfall (last hour): </span>
+            <span>{mmToIn(snow['1h'])}in</span>
+          </li>
+        )}
+
+        {has(windSpeed) && (
+          <li>
+            <span>Wind: </span>
+            <span>
+              {windDirection && getWindDirection(windDirection)}, {windSpeed}{' '}
+              mph
+            </span>
+          </li>
+        )}
+        {has(gust) && (
+          <li>
+            <span>Wind Gusts: </span>
+            <span>{gust} mph</span>
+          </li>
+        )}
+        {has(pressure) && (
+          <li>
+            <span>Pressure: </span>
+            <span>{Math.round(getInHg(pressure) * 100) / 100} inHg</span>
+          </li>
+        )}
+        {has(cloudCover) && (
+          <li>
+            <span>Cloud cover: </span>
+            <span>{cloudCover}%</span>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
