36042164c49157acdd52ec84e7f3461ac3a59112
jonrutter <contact@jonrutter.com>
2021-10-29 16:48:39 -0400

    Break geolocationFetch into separate functions

--- src/api.js
+++ src/api.js
@@ -1,19 +1,22 @@
-export const geolocationFetch = (type = 'weather') => {
+export const getLocationFromBrowser = () => {
   return new Promise((resolve, reject) => {
-    const success = ({ coords }) => {
-      const { latitude: lat, longitude: lon } = coords;
-      fetch(`/.netlify/functions/api?type=${type}&lat=${lat}&lon=${lon}`)
-        .then((response) => response.json())
-        .then((data) => resolve(data))
-        .catch((err) => reject(err));
-    };
+    const success = ({ coords }) => resolve(coords);
     const error = (err) => reject(err);
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
     } else {
-      reject('Geolocation not authorized');
+      reject('No geolocation available');
     }
+  });
+};
+
+export const fetchWeather = ({ latitude, longitude }) => {
+  return new Promise((resolve, reject) => {
+    fetch(`/.netlify/functions/weather?&lat=${latitude}&lon=${longitude}`)
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
   });
 };
 
