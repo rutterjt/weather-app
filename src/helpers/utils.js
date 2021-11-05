4fe89033035939a81933fb90595c4da94362e56a
jonrutter <contact@jonrutter.com>
2021-11-05 16:14:10 -0400

    isEmpty util function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,3 +2,13 @@ export const isInRange =
   (num) =>
   ([min, max]) =>
     num >= min && num <= max;
+
+export const isEmpty = (obj) => {
+  /* takes an object or array as an argument, returns whether the object or array has no members */
+  if (obj && Array.isArray(obj)) return obj.length === 0;
+  return (
+    obj &&
+    Object.keys(obj).length === 0 &&
+    Object.getPrototypeOf(obj) === Object.prototype
+  );
+};
