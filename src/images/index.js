c16f4fe1d9d9e61a6213fa7a6ca4709ea6fc3f0a
Jon Rutter <contact@jonrutter.com>
2022-01-18 20:49:48 -0500

    Interface for importing inline svgs

---
+++ src/images/index.js
@@ -0,0 +1,29 @@
+import { ReactComponent as Thunderstorm } from './thunderstorm.svg';
+import { ReactComponent as Rain } from './rain.svg';
+import { ReactComponent as Mix } from './mix.svg';
+import { ReactComponent as Snow } from './snow.svg';
+import { ReactComponent as Hail } from './hail.svg';
+import { ReactComponent as Fog } from './fog.svg';
+import { ReactComponent as Sun } from './sun.svg';
+import { ReactComponent as MoonStars } from './moon-stars.svg';
+import { ReactComponent as SunClouds } from './sun-clouds.svg';
+import { ReactComponent as MoonClouds } from './moon-clouds.svg';
+import { ReactComponent as LightClouds } from './light-clouds.svg';
+import { ReactComponent as HeavyClouds } from './heavy-clouds.svg';
+import { ReactComponent as Rainbow } from './rainbow.svg';
+
+export {
+  Thunderstorm,
+  Rain,
+  Mix,
+  Snow,
+  Hail,
+  Fog,
+  Sun,
+  MoonStars,
+  SunClouds,
+  MoonClouds,
+  LightClouds,
+  HeavyClouds,
+  Rainbow,
+};
