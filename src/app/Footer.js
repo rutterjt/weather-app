b864efc85fd1e92953072f75cf38ec3b519c99b0
Jon Rutter <contact@jonrutter.com>
2022-01-18 20:46:30 -0500

    Footer

---
+++ src/app/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  text-align: center;
+  padding-top: 2rem;
+`;
+
+const Copyright = styled.p`
+  font-size: 0.9rem;
+  font-weight: bold;
+`;
+
+const Link = styled.a`
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`;
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <Copyright>
+        Copyright {new Date().getFullYear()},{' '}
+        <Link
+          href="https://github.com/jonrutter"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jon Rutter
+        </Link>
+        .
+      </Copyright>
+    </StyledFooter>
+  );
+};
+
+export default React.memo(Footer);
