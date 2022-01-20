f1beff13ff3aa3b494fd7fb78d708d7ba1951763
Jon Rutter <contact@jonrutter.com>
2022-01-20 17:07:43 -0500

    Title styled component

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  margin-bottom: 3rem;
+  text-align: center;
+  font-family: 'Open Sans';
+  font-weight: 900;
+  @media screen and (min-width: 768px) {
+    font-size: 3rem;
+  }
+`;
+
+export default Title;
