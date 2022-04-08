aa3c60a219bea4eff77bfa975a593e0cc2c9ea46
Jon Rutter <contact@jonrutter.com>
2022-04-07 21:55:13 -0400

    Clear styled-components theme files

--- src/styles/breakpoints.js
+++
@@ -1,7 +0,0 @@
-export const breakpoints = {
-  xs: '0px',
-  sm: '400px',
-  md: '768px',
-  lg: '992px',
-  xl: '1200px',
-};
--- src/styles/shadow.js
+++
@@ -1,2 +0,0 @@
-export const shadow =
-  'box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.05), 0px 4px 4px rgba(0, 0, 0, 0.08), 0px 10px 10px rgba(0, 0, 0, 0.12);';
