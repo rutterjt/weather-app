44ca3f0bcef4c7769eedd551989a9f04ca9ab4e3
Jon Rutter <contact@jonrutter.io>
2022-07-15 17:32:17 -0400

    Comment out unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
 import '@fontsource/open-sans';
 import '@fontsource/nunito';
 
-import { worker } from './api/server';
+// import { worker } from './api/server';
 
-import { store } from './app/store';
+// import { store } from './app/store';
 
-import App from './App';
-import { saveState } from './app/localStorage';
+import { App } from './app';
+// import { saveState } from './app/localStorage';
 
 import './styles/index.css';
 
-store.subscribe(() => {
-  saveState(store.getState().location);
-});
+// store.subscribe(() => {
+// saveState(store.getState().location);
+// });
 
-async function start() {
-  // Start our mock API server
-  await worker.start({ onUnhandledRequest: 'bypass' });
+// async function start() {
+//   // Start our mock API server
+//   await worker.start({ onUnhandledRequest: 'bypass' });
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }
 
-start();
+// start();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
